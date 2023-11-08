70285844108ce3e8c7e1bb9c0aa0a9e7ec22bb84
Nghia <nghiatrinhtrong@gmail.com>
2023-11-08 17:15:57 +0700

    ngoc

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ngọc Egg';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100006086535090';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc mừng sinh nhật người yêu dễ thương và ngoan hiền dịu nhất quả đất của tôi. Cuộc sống sẽ rất buồn tẻ nếu vắng em. Hãy cùng nhau trải qua nhiều dịp sinh nhật nữa nhé!.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
